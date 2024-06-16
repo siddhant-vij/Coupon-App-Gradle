36b884a4c954e21e4887766137d2c7ebefea8a3d
Siddhant Vij <siddhantvij201@gmail.com>
2024-06-16 21:39:16 +0530

    demo: data, integration & service layers
    - temp project modules

---
+++ data/src/main/java/com/couponapp/App.java
@@ -0,0 +1,11 @@
+package com.couponapp;
+
+public class App {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sum(5, 5));
+  }
+}
